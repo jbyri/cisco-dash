a1194a39d10ab24a31158b21fa9896d54bd006bc
Decoded4620 <barcher4620@gmail.com>
2017-06-21 23:17:18 -0700

    Add a README to assets to outline its structure and purpose. Move customer data up a level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import {ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   selector: 'cisco-dash-app',
   templateUrl: './assets/templates/layouts/app-layout.html',
-  styleUrls: ['./assets/styles/app.css', '/node_modules/bootstrap/dist/boostrap.css', '/node_modules/bootstrap/dist/boostrap-grid.css']
+  styleUrls: [
+    './assets/styles/app.css',
+    '/node_modules/bootstrap/dist/boostrap.css',
+    '/node_modules/bootstrap/dist/boostrap-grid.css'
+  ]
 })
 export class AppComponent  { name = 'Angular'; }
