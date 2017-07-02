9683ebfaad240f8723df68d39bb8ec74f3a8ccaa
Itay Elgazar <Itayekk1@gmail.com>
2017-07-02 12:25:54 +0300

    bs-config.js modified

--- bs-config.js
+++ bs-config.js
@@ -1,6 +1,9 @@
 module.exports = {
     port: process.env.PORT,
     server:{
-        baseDir: "./src"
+        baseDir: "./src",
+        routes: {
+            "/node_modules": "node_modules"
+        }
     }
 };
