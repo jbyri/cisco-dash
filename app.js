22d515fe0808685dd022ca605068bccb1af6c4d9
Itay Elgazar <Itayekk1@gmail.com>
2017-07-02 22:45:47 +0300

    fix attempt

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app     = express();
 var path = require('path');
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(path.join(__dirname, '/client')));
-app.use(express.static(path.join(__dirname, '/client/src')));
+app.use(express.static(path.join(__dirname, '/src')));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 //For avoidong Heroku $PORT error
 app.listen(app.get('port'), function() {
