ec216e6439008c48e05c420b536e0d72fcdfd6cf
Itay Elgazar <Itayekk1@gmail.com>
2017-07-02 12:41:00 +0300

    bs-config.js

--- bs-config.js
+++ bs-config.js
@@ -1,14 +1,9 @@
 module.exports = {
     port: process.env.PORT,
-    open: false,
-    logLevel: "silent",
     server: {
         baseDir: "src",
         routes: {
         "/node_modules": "node_modules"
-        },
-        middleware: {
-        "0": null
         }
     }
 };
