bde2e904c2db72963df4f2b64509c51489e11481
Itay Elgazar <Itayekk1@gmail.com>
2017-07-02 14:17:05 +0300

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app     = express();
 var path = require('path');
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, '/client')));
 app.use(express.static(path.join(__dirname, '/client/src')));
 app.use('/node_modules/tether/dist/css/',express.static(path.join(__dirname,'/node_modules/node_modules/tether/dist/css/')));
 //For avoidong Heroku $PORT error
