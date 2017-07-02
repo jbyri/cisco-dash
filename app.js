b7764d97f4bb73824949797683bd2538053260b4
Itay Elgazar <Itayekk1@gmail.com>
2017-07-02 22:06:53 +0300

    test1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, '/client')));
 app.use(express.static(path.join(__dirname, '/client/src')));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/node_modules', express.static(__dirname + '/app/node_modules'));
 //For avoidong Heroku $PORT error
 app.listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
