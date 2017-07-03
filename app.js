b93c0fa23e47b6b24372e55eeca3b84b1a18fed3
wnamen <williamjosephnamen@gmail.com>
2017-07-02 19:03:47 -0700

    json file

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app     = express();
 var path = require('path');
-app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.join(__dirname, '/src')));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/src/index.html')
+})
+
+app.set('port', (process.env.PORT || 5000));
 //For avoidong Heroku $PORT error
 app.listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
