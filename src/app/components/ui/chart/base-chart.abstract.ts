134d3cc1dc9062d5072ffcaa6d3583ad0d8b84ba
Decoded4620 <barcher4620@gmail.com>
2017-07-05 10:30:17 -0700

    Adding base-chart.abstract.ts, a base interface for all charts to extend

--- src/app/components/ui/chart/base-chart.abstract.ts
+++ src/app/components/ui/chart/base-chart.abstract.ts
@@ -1,3 +1,14 @@
+/**
+ * Used to provide an inherited convenience for Chart Components of any type.
+ * this class contains on the basic needs for all charts. Specific charts that extend
+ * this class will provide their own custom logic to render as needed.
+ *
+ * This interface is not tied to some specific charting framework. The idea
+ * is to support any chart framework. The chart components are to provide
+ * the logic necessary for the chosen charting framework. Here we provide a simple
+ * division between view data and model data so that Chart components contain the data
+ * required to render.
+ */
 export interface ChartComponent {
   /**
    * Data passed directly into the chart
@@ -6,7 +17,7 @@ export interface ChartComponent {
 
 
   /**
-   * Data that is used to configure the chart, and contais the chartData and
+   * Data that is used to configure the chart, and contains the chartData and
    * chart Data's labels.
    */
   chartUIData : object;
