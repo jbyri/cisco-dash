3a1f05e95dc1f56e1a125061ec57d983bd3ed79d
Decoded4620 <barcher4620@gmail.com>
2017-07-08 22:27:01 -0700

    insure we aren't logging into local host when doing sign up. we need to use window.location or we are just signing into our own local boxes, not the aws instance. Also this means people like James would NOT be able to login

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -6,12 +6,20 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthenticationService {
     constructor(private http: Http) { }
+
  
     login(email: string, password: string) : Observable<boolean> {
-        return this.http.post('http://localhost:5000/api/user/login', { email, password })
+        let serverUrl = window.location.origin;
+        if(serverUrl.indexOf('localhost:') > -1) {
+            serverUrl = 'http://localhost';
+        }
+        return this.http.post(serverUrl + ':5000/api/user/login', { email, password })
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let user = response.json();
+
+                console.log("USER", user);
+
                 if (user && user.token) {
                   // store user details and jwt token in local storage to keep user logged in between page refreshes
                   localStorage.setItem('token', JSON.stringify(user.token));
@@ -24,10 +32,16 @@ export class AuthenticationService {
     }
 
     signup(firstName: string, lastName: string, email: string, password: string) : Observable<boolean> {
-        return this.http.post('http://localhost:5000/api/user/signup', { firstName, lastName, email, password })
+        let serverUrl = window.location.origin;
+        if(serverUrl.indexOf('localhost:') > -1) {
+            serverUrl = 'http://localhost';
+        }
+        return this.http.post(serverUrl + ':5000/api/user/signup', { firstName, lastName, email, password })
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let user = response.json();
+                console.log('signup: ', user);
+
                 if (user && user.token) {
                   // store user details and jwt token in local storage to keep user logged in between page refreshes
                   localStorage.setItem('token', JSON.stringify(user.token));
