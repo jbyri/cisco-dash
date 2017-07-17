275469b7628b3c492e55a4b1c5cdd129f3f6d497
decoded4620 <ubuntu@ip-172-31-21-87.ec2.internal>
2017-07-17 03:10:44 +0000

    add the cp step to the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Runs the install script, then links node moduls
-rm -R /var/www/html/* && npm install && gulp deployRelease && ln -s ~/git/cisco-dash/node_modules /var/www/html/node_modules
+rm -R /var/www/html/* && npm install && npm run deploy:release && cp -R ~/git/cisco-dash/dist/* /var/www/html/ && ln -s ~/git/cisco-dash/node_modules /var/www/html/node_modules
