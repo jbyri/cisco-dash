a0b8ad684157e2f23ac0e7d988b66770966a743a
decoded4620 <ubuntu@ip-172-31-21-87.ec2.internal>
2017-07-17 06:15:17 +0000

    working on clean + deploy added missing installs to package json

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npm install && gulp deployRelease
 echo "Upgrade NPM and distribution completed"
 cp -R ~/git/cisco-dash/dist/* /var/www/html/
 echo "Copied all resources from dist to http root"
-ln -s ~/git/cisco-dash/node_modules /var/www/html/node_modules
+ln -sT ~/git/cisco-dash/node_modules /var/www/html/node_modules
 echo "linked node modules"
-cd /var/www/html/ && start-mongod.sh && start-server.sh && cd ~/git/cisco-dash
+cd /var/www/html/ && ./start-mongod.sh && ./start-server.sh
 echo "restarted mongo and passport service."
