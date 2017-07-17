3765843d727b4c7bdc3c2dc0914030b98014722c
Decoded4620 <barcher4620@gmail.com>
2017-07-16 22:33:06 -0700

    seperate clean and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Runs the install script, then links node moduls
-cd /var/www/html/ && stop-server.sh && stop-mongod.sh && cd ~/git/cisco-dash
-echo "finished stopping services"
-rm -R /var/www/html/*
-echo "cleaned HTTP Root Directory"
 npm install && gulp deployRelease
 echo "Upgrade NPM and distribution completed"
-cp -R ~git/cisco-dash/dist/* /var/www/html/
+cp -R ~/git/cisco-dash/dist/* /var/www/html/
 echo "Copied all resources from dist to http root"
 ln -s ~/git/cisco-dash/node_modules /var/www/html/node_modules
 echo "linked node modules"
