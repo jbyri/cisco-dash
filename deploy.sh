7bab30bc67de31660371ef9e9a86ed11b254c074
Decoded4620 <barcher4620@gmail.com>
2017-07-16 19:59:24 -0700

    minor fix to link script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Runs the install script, then links node moduls
-rm -R /var/www/html/* && npm install && gulp deployRelease && ln -s ./node_modules /var/www/html/node_modules
+rm -R /var/www/html/* && npm install && gulp deployRelease && ln -s ~/git/cisco-dash/node_modules /var/www/html/node_modules
